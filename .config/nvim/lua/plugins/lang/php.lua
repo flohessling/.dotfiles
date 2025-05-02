ac738ebccc1ceecc2ec21fdf2ec66f33d75926ac
Flo Hessling <f.hessling@shopware.com>
2025-05-02 15:18:35 +0200

    feat: update nvim settings for php, update plugins

--- .config/nvim/lua/plugins/lang/php.lua
+++ .config/nvim/lua/plugins/lang/php.lua
@@ -1,53 +1,53 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, {
-				"php",
-				"phpdoc",
-			})
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, {
-				-- "intelephense",
-				"phpactor",
-				"phpstan",
-				"php-cs-fixer",
-				"easy-coding-standard",
-				"twiggy-language-server",
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			servers = {
-				-- intelephense = {},
-				phpactor = {},
-				twiggy_language_server = {},
-			},
-		},
-	},
-	{
-		"stevearc/conform.nvim",
-		opts = function(_, opts)
-			opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft, {
-				php = { "easy-coding-standard" },
-			})
-		end,
-	},
-	{
-		"mfussenegger/nvim-lint",
-		opts = function(_, opts)
-			opts.linters_by_ft = vim.tbl_deep_extend("force", opts.linters_by_ft, {
-				php = { "php", "phpstan" },
-			})
-		end,
-	},
-	{
-		"nelsyeung/twig.vim",
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, {
+                "php",
+                "phpdoc",
+            })
+        end,
+    },
+    {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, {
+                "intelephense",
+                -- "phpactor",
+                "phpstan",
+                "php-cs-fixer",
+                "easy-coding-standard",
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                intelephense = {
+                    init_options = {
+                        globalStoragePath = (os.getenv("XDG_DATA_HOME") or "") .. "/intelephense",
+                        licenseKey = (os.getenv("XDG_CONFIG_HOME") or "") .. "/intelephense/license.txt",
+                    },
+                },
+                -- phpactor = {},
+            },
+        },
+    },
+    {
+        "stevearc/conform.nvim",
+        opts = function(_, opts)
+            opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft, {
+                php = { "php-cs-fixer" },
+            })
+        end,
+    },
+    {
+        "mfussenegger/nvim-lint",
+        opts = function(_, opts)
+            opts.linters_by_ft = vim.tbl_deep_extend("force", opts.linters_by_ft, {
+                php = { "php", "phpstan" },
+            })
+        end,
+    },
 }

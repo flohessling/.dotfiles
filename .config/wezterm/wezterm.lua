85959b2eb3c8d6e6a8790025dd9a09f9b78b192d
Flo Hessling <f.hessling@shopware.com>
2025-06-12 10:12:25 +0200

    chore: fix Brewfile casks, adjust wezterm font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,8 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.default_prog = { "/opt/homebrew/bin/zsh" }
-config.font = wezterm.font("Hack Nerd Font")
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+-- config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 12.0
 config.pane_focus_follows_mouse = true
 config.window_decorations = "RESIZE"
